0f1205cbfd8eb92f244724e65842dea96b4cab6c
alaaibrahem1 <alaaibrahem970@gmail.com>
2025-02-23 12:49:31 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Alaa Ibrahem',
+    default: 'Alaa Ibrahem - Front-End Developer & Travel Tech Enthusiast',
+  },
+  description:
+  'I’m Alaa, a Front-End Developer passionate about building seamless travel booking experiences. Specializing in JavaScript and modern web technologies.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
